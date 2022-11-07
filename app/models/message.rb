6b847b2a4dc14b2a761e9d40437be2064ad6e531
Aleksei Iurin <19alex85@gmail.com>
2022-11-07 15:47:07 +0300

    add messages form and more

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,4 +1,4 @@
 class Message < ApplicationRecord
-  belongs_to :room
+  belongs_to :room, optional: true
   belongs_to :user
 end
--- app/models/room.rb
+++ app/models/room.rb
@@ -3,11 +3,13 @@ class Room < ApplicationRecord
 
   before_create :generate_token
 
-  def generate_token
-    self.token = SecureRandom.hex(2)
-  end
-
   def to_param
     token
+  end
+
+  private
+
+  def generate_token
+    self.token = SecureRandom.hex(2)
   end
 end
